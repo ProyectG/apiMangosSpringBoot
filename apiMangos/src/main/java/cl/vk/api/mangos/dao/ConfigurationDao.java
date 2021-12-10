8a2974eb69c6d958d5f4ac31cba6d1f9962d6d0b
ProyectG <vkey@athena>
2021-12-09 22:35:12 -0300

    La seguridad queda configurable.

--- apiMangos/src/main/java/cl/vk/api/mangos/dao/ConfigurationDao.java
+++ apiMangos/src/main/java/cl/vk/api/mangos/dao/ConfigurationDao.java
@@ -1,17 +1,32 @@
 package cl.vk.api.mangos.dao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
 import cl.vk.api.mangos.conf.ServerParam;
 import cl.vk.api.mangos.dto.Configuration;
 import cl.vk.api.mangos.dto.ConfigurationResponse;
 import cl.vk.api.mangos.dto.GetConfiguration;
 import cl.vk.api.mangos.parameter.ApiParameters;
+import cl.vk.mangos.transmision.ConfigurationUtils;
 import cl.vk.mangos.transmision.JWTutils;
 
 public class ConfigurationDao extends JWTutils {
 
+	final Logger LOGGER = LoggerFactory.getLogger(ConfigurationDao.class);
+	
+	@Value("${mangos.security}")
+	protected Boolean seguridad;
+	
+	public ConfigurationDao() {
+		if(ConfigurationUtils.getPropiedades().get("mangos.security") != null)
+			this.seguridad = (Boolean) ConfigurationUtils.getPropiedades().get("mangos.security");
+	}
+	
 	public ConfigurationResponse setConfiguration(String jwt, Configuration input) {
 		ConfigurationResponse response = new ConfigurationResponse();
-		if (validarJWT(jwt)) {
+		if (!this.seguridad || validarJWT(jwt)) {
 			try {
 				ServerParam.setPass(input.getPass());
 				ServerParam.setPort(input.getPort());
@@ -24,6 +39,7 @@ public class ConfigurationDao extends JWTutils {
 			} catch (Exception e) {
 				response.setCod("1");
 				response.setDsc("Error al setear las configuraciones");
+				LOGGER.error("Error al setear los parametros ", e);
 			}
 		} else
 		{
@@ -35,7 +51,7 @@ public class ConfigurationDao extends JWTutils {
 
 	public GetConfiguration getConf(String jwt) {
 		GetConfiguration response = new GetConfiguration();
-		if (validarJWT(jwt)) {
+		if (!this.seguridad || validarJWT(jwt)) {
 			ApiParameters parametros = new ApiParameters();
 			parametros.setPass(ServerParam.getPass());
 			parametros.setPort(ServerParam.getPort());
